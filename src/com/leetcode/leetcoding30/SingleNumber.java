66394f306700bcf285a83e1b8420cece77b91ec2
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-03 17:55:05 +0530

    Single Number

--- src/com/leetcode/leetcoding30/SingleNumber.java
+++ src/com/leetcode/leetcoding30/SingleNumber.java
@@ -1,8 +1,33 @@
 package com.leetcode.leetcoding30;
 
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ *
+ */
 public class SingleNumber {
-    // XOR Gate, worked 
-    // Hehe, it just came out of nowhere and striked
+
+
+    /**
+     *
+     * XOR Gate,
+     *
+     * 1 XOR 1 is always 0
+     * Finally we endup having only the single number
+     *
+     */
     public static int singleNumber(int[] nums) {
         int singleNumber = 0;
         for (int i = 0; i < nums.length ; i++)
