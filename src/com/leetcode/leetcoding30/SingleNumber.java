9ecec112511b8edfa3106598111edc614b8b8cf8
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-03 17:32:50 +0530

    Initial commit

---
+++ src/com/leetcode/leetcoding30/SingleNumber.java
@@ -0,0 +1,20 @@
+package com.leetcode.leetcoding30;
+
+public class SingleNumber {
+
+    public static int singleNumber(int[] nums) {
+        int singleNumber = 0;
+        for (int i = 0; i < nums.length ; i++)
+          singleNumber = singleNumber ^ nums[i];
+        return singleNumber;
+    }
+
+
+    public static void main(String[] args) {
+        int[] TEST_CASE_01 = new int[]{2, 2, 1};
+        int[] TEST_CASE_02 = new int[]{4,1,2,1,2};
+        System.out.println(singleNumber(TEST_CASE_01));
+
+    }
+
+}
