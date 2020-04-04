c43bebd1337e92b91276874ce3d1785548172c2c
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-04 18:57:46 +0530

    MoveZeroes

---
+++ src/com/leetcode/leetcoding30/MoveZeroes.java
@@ -0,0 +1,44 @@
+package com.leetcode.leetcoding30;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Note:
+ * <p>
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ */
+public class MoveZeroes {
+
+
+
+    public static void moveZeroes(int[] nums) {
+
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] TEST_CASE_001 = { 1,2,3,4,5};
+        moveZeroes(TEST_CASE_001);
+        System.out.println(Arrays.toString(TEST_CASE_001));
+    }
+}
