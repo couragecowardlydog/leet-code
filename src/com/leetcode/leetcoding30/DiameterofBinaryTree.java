6a93995fd4f5cedf4002aa3250e8b81f4be9faa5
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-12 10:27:18 +0530

    DiameterofBinaryTree : Reducing runtime

--- src/com/leetcode/leetcoding30/DiameterofBinaryTree.java
+++ src/com/leetcode/leetcoding30/DiameterofBinaryTree.java
@@ -2,19 +2,25 @@ package com.leetcode.leetcoding30;
 
 /**
  * Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
- *
+ * <p>
  * Example:
  * Given a binary tree
- *           1
- *          / \
- *         2   3
- *        / \
- *       4   5
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
  * Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
- *
+ * <p>
  * Note: The length of path between two nodes is represented by the number of edges between them.
+ *
+ *
+ * 1st Submission : 116ms/45MB
+ * 2nd Submission : 12ms/40MB => No logic change, just cleaned the code
  */
 public class DiameterofBinaryTree {
+
+
 
     public class TreeNode {
         int val;
@@ -39,26 +45,14 @@ public class DiameterofBinaryTree {
             return 0;
 
         // Distance can be any two nodes, need not to be the root's right and left
-        int fromRoot = lenghtOfTree(root.right) + lenghtOfTree(root.left);
-        int itsRight = diameterOfBinaryTree(root.left);
-        int itsLeft = diameterOfBinaryTree(root.right);
-
-        if((fromRoot >= itsLeft) && (fromRoot >= itsRight))
-            return  fromRoot;
-        if((itsLeft >= fromRoot) && (itsLeft >= itsRight))
-            return itsLeft;
-        return itsRight;
+        int insideChilds = Math.max(diameterOfBinaryTree(root.left), diameterOfBinaryTree(root.right));
+        return Math.max(lenghtOfTree(root.right) + lenghtOfTree(root.left), insideChilds);
     }
 
 
     public int lenghtOfTree(TreeNode root) {
         if (null == root)
             return 0;
-
-        int left = 1 + lenghtOfTree(root.left);
-        int right = 1 + lenghtOfTree(root.right);
-
-        return left > right ? left : right;
-
+        return Math.max(lenghtOfTree(root.left), lenghtOfTree(root.right)) + 1;
     }
 }
