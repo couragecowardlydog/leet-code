be82bd8ff0bbe4531618f6fbf788d12b968be2d7
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-14 16:00:18 +0530

    Update ContiguousArray.java

--- src/com/leetcode/leetcoding30/ContiguousArray.java
+++ src/com/leetcode/leetcoding30/ContiguousArray.java
@@ -20,16 +20,7 @@ import java.util.Map;
 public class ContiguousArray {
 
 
-    /*public static int findMaxLength(int[] nums) {
-        int sum = 0;
-        int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if ((sum == ((i + 1) / 2)) && ((i + 1) % 2 == 0))
-                result = i + 1;
-        }
-        return result;
-    }*/
+
 
     /**
      *  Before cleaning code
