8dd55fe80d3af8e77c08678d0f4dff014f558ac8
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-21 21:30:04 +0530

    Update MinimumPathSum.java

--- src/com/leetcode/leetcoding30/MinimumPathSum.java
+++ src/com/leetcode/leetcoding30/MinimumPathSum.java
@@ -1,23 +1,20 @@
 package com.leetcode.leetcoding30;
 
 /**
- *
  * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
- *
+ * <p>
  * Note: You can only move either down or right at any point in time.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input:
  * [
- *   [1,3,1],
- *   [1,5,1],
- *   [4,2,1]
+ * [1,3,1],
+ * [1,5,1],
+ * [4,2,1]
  * ]
  * Output: 7
  * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
- * 
- *
  */
 public class MinimumPathSum {
 
@@ -26,25 +23,22 @@ public class MinimumPathSum {
     }
 
     /**
-     *
      * :( Time Limit started execeeding from test case 21
      */
     public static int minSum(int[][] grid, int i, int j) {
-        if (i > grid.length - 1)
-            return Integer.MAX_VALUE - grid[i - 1][j];
-        if (j > grid[0].length - 1)
-            return Integer.MAX_VALUE - grid[i][j - 1];
+        if (i > grid.length - 1 || j > grid[0].length - 1)
+            return Integer.MAX_VALUE;
         if (i == grid.length - 1 && j == grid[0].length - 1)
             return grid[i][j];
-        return Math.min(grid[i][j] + minSum(grid, i + 1, j), grid[i][j] + minSum(grid, i, j + 1));
+        return grid[i][j] + Math.min(minSum(grid, i + 1, j), minSum(grid, i, j + 1));
     }
 
 
     public static void main(String[] args) {
         int[][] mxn = {
-                {1, 3, 1},
+                {1, 3, 19},
                 {1, 5, 1},
-                {4, 2, 1}
+                {14, 2, 1}
         };
 
         System.out.println(minPathSum(mxn));
