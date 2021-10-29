67da16d428bb76b030ec67c03303fd25e0c52f76
Vivekanandan <vivekanandansakthivelu@gmail.com>
2021-10-29 19:14:40 +0530

    feat: Data Structure 1

---
+++ src/com/leetcode/datastructures/TwoSum.java
@@ -0,0 +1,72 @@
+package com.leetcode.datastructures;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author vivekanandan.sakthiv on Oct 2021
+ *
+ * 1. Two Sum
+ * Easy
+ *
+ * 25807
+ *
+ * 837
+ *
+ * Add to List
+ *
+ * Share
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * Example 2:
+ *
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * Example 3:
+ *
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * Only one valid answer exists.
+ *
+ *
+ *
+ */
+
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i]))
+                return new int[]{hashMap.get(target - nums[i]), i};
+            hashMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+    }
+}
