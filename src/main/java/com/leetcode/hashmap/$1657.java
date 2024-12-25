23bbaa7a63891d8e250ccd98baa129b6643b3dc3
Vivekanandan <vivekanandan.sakthivelu@toyotaconnected.co.in>
2024-12-25 10:59:58 +0900

    chore: optimize runtime

--- src/main/java/com/leetcode/hashmap/$1657.java
+++ src/main/java/com/leetcode/hashmap/$1657.java
@@ -8,24 +8,43 @@ import java.util.Objects;
 
 public class $1657 {
 
+    int[] sourceArr = new int[26];
+    int[] targetArr = new int[26];
+
+    public void clear() {
+        Arrays.fill(sourceArr, 0);
+        Arrays.fill(targetArr, 0);
+    }
+
     public boolean closeStrings(String word1, String word2) {
+
 
         // Base case 1 : String length is different
         if (word1.length() != word2.length())
             return false;
 
+        clear();
+
         Map<Character, Integer> map1 = new HashMap<>();
         Map<Character, Integer> map2 = new HashMap<>();
 
-        for (int i = 0; i < word1.length(); i++)
+        for (int i = 0; i < word1.length(); i++) {
             map1.put(word1.charAt(i), map1.getOrDefault(word1.charAt(i), 0) + 1);
+            sourceArr[word1.charAt(i) - 'a']++;
+        }
 
-        for (int i = 0; i < word2.length(); i++)
+        for (int i = 0; i < word2.length(); i++) {
             map2.put(word2.charAt(i), map2.getOrDefault(word2.charAt(i), 0) + 1);
+            targetArr[word2.charAt(i) - 'a']++;
+        }
 
-        // Base case 2: has different character from each other
-        if (!map2.keySet().containsAll(map1.keySet()))
-            return false;
+        for (int i = 0; i < 26; i++) {
+            if (sourceArr[i] == targetArr[i])
+                continue;
+            if (!(sourceArr[i] > 0 && targetArr[i] > 0)) {
+                return false;
+            }
+        }
 
         for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
             int value = entry.getValue();
