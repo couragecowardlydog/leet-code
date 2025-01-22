b83e26c1e70755fda633f18be2f2a312671a1389
vivtcjp <v.sakthivel@mail.toyotaconnected.co.jp>
2025-01-22 17:55:14 +0900

    feat: 1456

---
+++ src/main/java/com/leetcode/slidingwindow/$1456.java
@@ -0,0 +1,23 @@
+package com.leetcode.slidingwindow;
+
+public class $1456 {
+
+    public int maxVowels(String s, int k) {
+        int result = 0;
+        int i;
+        char[] chars = s.toCharArray();
+        int count = 0;
+        for (i = 0; i < k; i++) {
+            count += isVowel(chars[i]);
+        }
+        result = Math.max(result, count);
+        for (i = k; i < chars.length - k; i++) {
+            result = Math.max(result, count + isVowel(chars[i]) - isVowel(chars[i - k]));
+        }
+        return result;
+    }
+
+    private int isVowel(char c) {
+        return ('a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c) ? 1 : 0;
+    }
+}
---
+++ src/test/java/com/leetcode/slidingwindow/$1456Test.java
@@ -0,0 +1,33 @@
+package com.leetcode.slidingwindow;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class $1456Test {
+
+    $1456 solution = new $1456();
+
+    public Stream<Arguments> parameters() {
+        return Stream.of(
+                arguments("abciiidef", 3, 2),
+                arguments("aeiou", 2, 2),
+                arguments("leetcode", 3, 2)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("parameters")
+    public void smokeTest(String s, int k, int expected) {
+        Assertions.assertEquals(expected, solution.maxVowels(s, k));
+    }
+}
