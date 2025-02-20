a6b483b666e6b8626de0b087772695689e925370
v.sakthivel@mail.toyotaconnected.co.jp <v.sakthivel@mail.toyotaconnected.co.jp>
2025-02-20 17:32:34 +0900

    Create $872.java

---
+++ src/main/java/com/leetcode/binarytree/$872.java
@@ -0,0 +1,41 @@
+package com.leetcode.binarytree;
+
+import java.util.Arrays;
+
+public class $872 {
+
+    public static int index = 0;
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        boolean result = true;
+        int[] rleaf = new int[128];
+        int[] lleaf = new int[128];
+        index = 0;
+        traverse(root1, rleaf);
+        index = 0;
+        traverse(root2, lleaf);
+
+        System.out.println(Arrays.toString(rleaf));
+        System.out.println(Arrays.toString(lleaf));
+        for (int i = 0; i < 128; i++) {
+            if (rleaf[i] != lleaf[i]) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    private void traverse(TreeNode root, int[] leaf) {
+        if (root.left == null && root.right == null)
+            leaf[index++] = root.val;
+        if (null != root.left)
+            traverse(root.left, leaf);
+        if (null != root.right)
+            traverse(root.right, leaf);
+    }
+
+
+}
