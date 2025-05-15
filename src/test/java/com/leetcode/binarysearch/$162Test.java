596d48ab279138187c61f63d3a9239433bbeb94e
v.sakthivel@mail.toyotaconnected.co.jp <v.sakthivel@mail.toyotaconnected.co.jp>
2025-05-15 17:17:27 +0900

    Create $162Test.java

---
+++ src/test/java/com/leetcode/binarysearch/$162Test.java
@@ -0,0 +1,29 @@
+package com.leetcode.binarysearch;
+
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class $162Test {
+
+    private final $162 solution = new $162();
+
+
+    public Stream<Arguments> inputs() {
+        return Stream.of(
+                Arguments.arguments(new int[]{1, 2, 3, 1}, 2)
+        );
+    }
+
+    @MethodSource("inputs")
+    @ParameterizedTest
+    public void test(int[] nums, int result) {
+        assertEquals(result, solution.findPeakElement(nums));
+    }
+}
