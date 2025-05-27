708efb3f6ea9bb73796f2a232a027f0b78523553
Vivekanandan <vivekanandan.sakthivelu@toyotaconnected.co.in>
2025-05-27 09:14:14 +0900

    Update $526.java

--- src/main/java/com/leetcode/backtracking/$526.java
+++ src/main/java/com/leetcode/backtracking/$526.java
@@ -6,35 +6,30 @@ import java.util.List;
 public class $526 {
 
     int[] arr;
-    int[] visited;
     List<Integer> permutations = new ArrayList<>();
     int result = 0;
 
     public int countArrangement(int n) {
         arr = new int[n];
-        visited = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
         permutation();
         return result;
     }
 
     public void permutation() {
-        if (permutations.size() == visited.length) {
+        if (permutations.size() == arr.length) {
             result++;
             return;
         }
         for (int j = 0; j < arr.length; j++) {
-            if (visited[j] == 1)
+            if (arr[j] == 1)
                 continue;
-            if (!validateConstraint(arr[j], permutations.size() + 1)) {
+            if (!validateConstraint(j + 1, permutations.size() + 1)) {
                 continue;
             }
-            visited[j] = 1;
-            permutations.add(arr[j]);
+            arr[j] = 1;
+            permutations.add(j + 1);
             permutation();
-            visited[j] = 0;
+            arr[j] = 0;
             permutations.remove(permutations.size() - 1);
         }
     }
