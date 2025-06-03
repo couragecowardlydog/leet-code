b7330b0755fdc26a3d67f292d1e75bab35386efd
v.sakthivel@mail.toyotaconnected.co.jp <v.sakthivel@mail.toyotaconnected.co.jp>
2025-06-03 15:24:27 +0900

    feat: $89 / TLE

---
+++ src/main/java/com/leetcode/backtracking/$89.java
@@ -0,0 +1,38 @@
+package com.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class $89 {
+
+    List<Integer> result = new ArrayList<>();
+
+    // time limit exceed
+    public List<Integer> TLE(int n) {
+        int x = 1;
+        result.add(0);
+        result.add(1);
+        int max = (int) Math.pow(2, n);
+        while (true) {
+            if (result.size() >= max)
+                break;
+            x = getNumber(x, 0);
+            result.add(x);
+        }
+
+        return result;
+    }
+
+    public int getNumber(int x, int i) {
+        int y = x ^ (1 << i);
+        if (result.contains(y))
+            return getNumber(x, i + 1);
+        return y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new $89().grayCode(1));
+    }
+
+
+}
