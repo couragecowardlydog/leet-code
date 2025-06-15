35a032a90f846da8e40b43108051e1cca0bccc2d
Vivekanandan <vivekanandan.sakthivelu@toyotaconnected.co.in>
2025-06-15 18:43:49 +0900

    $97

--- src/main/java/com/leetcode/dp/$97.java
+++ src/main/java/com/leetcode/dp/$97.java
@@ -4,57 +4,39 @@ import java.util.Arrays;
 
 public class $97 {
 
-    String a;
-    String b;
-    String c;
-    StringBuilder path = new StringBuilder();
-    int[][][] dp;
-    boolean found = false;
-
+    private Boolean[][] memo;
 
     public boolean isInterleave(String a, String b, String c) {
+        this.memo = new Boolean[a.length() + 1][b.length() + 1];
         if (a.length() + b.length() != c.length()) return false;
-        this.a = a;
-        this.b = b;
-        this.c = c;
-
-        int[][] aDp = new int[a.length()][a.length()];
-        int[][] bDp = new int[b.length()][b.length()];
-        for (int i = 0; i < aDp.length; i++) {
-            Arrays.fill(aDp[i], -1);
-        }
-        for (int i = 0; i < bDp.length; i++) {
-            Arrays.fill(bDp[i], -1);
-        }
-        this.dp = new int[][][]{aDp, bDp};
-
-
-        System.out.println(Arrays.deepToString(dp));
-        traverse(0, 1, 0, 0);
-        System.out.println("Final Path::" + found);
-        if (!found) traverse(0, 0, 0, 0);
-        System.out.println("Final Path::" + found);
-        return found;
+        return traverse(0, 0, a, b, c);
     }
 
-    public void traverse(int pathIndex, int sIndex, int aIndex, int bIndex) {
-        System.out.println(path);
-        if (path.toString().equals(c)) found = true;
-        if (found) return;
-        if (path.length() > c.length() || !path.toString().equalsIgnoreCase(c.substring(0, path.length()))) return;
-        if (aIndex >= a.length() && bIndex > b.length()) return;
-        String current = sIndex == 0 ? a : b;
-        int currentIndex = sIndex == 0 ? aIndex : bIndex;
 
-        for (int i = 0; i < (current.length() - currentIndex); i++) {
-            path.append(current.substring(currentIndex, currentIndex + i + 1));
-            traverse(pathIndex + 1, (sIndex == 0 ? 1 : 0), (aIndex + (sIndex == 0 ? (i + 1) : 0)), (bIndex + (sIndex == 0 ? 0 : (i + 1))));
-            path.delete(path.length() - (i + 1), path.length());
+    public boolean traverse(int row, int column, String a, String b, String c) {
+        if (row == a.length() && column == b.length())
+            return true;
+        if (memo[row][column] != null)
+            return memo[row][column];
+        boolean result = false;
+        int k = row + column;
+        char targetChar = c.charAt(k);
+        if (row < a.length() && a.charAt(row) == targetChar) {
+            result = traverse(row + 1, column, a, b, c);
         }
+        if (!result && column < b.length() && b.charAt(column) == targetChar) {
+            result = traverse(row, column + 1, a, b, c);
+        }
+        return memo[row][column] = result;
     }
+
 
     public static void main(String[] args) {
         $97 obj = new $97();
-        obj.isInterleave("abc", "def", "deafbc");
+        System.out.println(obj.isInterleave(
+                "aabcc",
+                "dbbca",
+                "aadbbcbcac"
+        ));
     }
 }
